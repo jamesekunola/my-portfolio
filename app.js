d28b6bd2d66981cb949ad63f55c992477b4c8ef4
James Ekunola <jamesekunola@gmail.com>
2023-03-12 11:11:41 +0100

    add more project to project page

--- app.js
+++ app.js
@@ -1,3 +1,85 @@
+const projects = [
+  {
+    img: "/assets/work_rest_country_api.jpg",
+    web: false,
+    name: "Country Api",
+    desc: `The rest country app is a valuable resource for anyone seeking to learn more about countries and
+their unique characteristics. Its user-friendly interface and powerful search and filtering
+capabilities make it a great tool for educators, researchers, and anyone with an interest in global
+affairs.`,
+    "source code":
+      "https://github.com/jamesekunola/rest-countries-api-with-color-theme-switcher-master",
+  },
+
+  {
+    img: "/assets/quiz_app.png",
+    web: false,
+    name: "Quiz App",
+    desc: ` The app works by presenting the user with a series of questions, retrieved from an API, along with
+    multiple choice answers. The user can select an answer for each question, and the app will keep
+    track of their score as they progress through the quiz.`,
+    "source code": "https://github.com/jamesekunola/quiz-app",
+  },
+
+  {
+    img: "/assets/todo.jpg",
+    web: false,
+    name: "Todo App",
+    desc: `  A classic todo app with a few twists! This app includes a dark/light theme toggle Whether you're a
+    busy professional or just looking to stay organized in your personal life, a todo app can be a
+    valuable tool for achieving your goals and increasing your productivity.`,
+    "source code": "https://github.com/jamesekunola/todo-app-main",
+  },
+
+  {
+    img: "/assets/ip-track.jpg",
+    web: false,
+    name: "ip address web tracker",
+    desc: ` The app works by presenting the user with a series of questions, retrieved from an API, along with
+    multiple choice answers. The user can select an answer for each question, and the app will keep
+    track of their score as they progress through the quiz.`,
+    "source code": "https://github.com/jamesekunola/quiz-app",
+  },
+  {
+    img: "/assets/int-rating.jpg",
+    web: false,
+    name: "interactive rating app",
+    desc: ` The app works by presenting the user with a series of questions, retrieved from an API, along with
+    multiple choice answers. The user can select an answer for each question, and the app will keep
+    track of their score as they progress through the quiz.`,
+    "source code": "https://github.com/jamesekunola/quiz-app",
+  },
+  {
+    img: "/assets/advice-generator.jpg",
+    web: false,
+    name: "Advice generating app",
+    desc: ` The app works by presenting the user with a series of questions, retrieved from an API, along with
+    multiple choice answers. The user can select an answer for each question, and the app will keep
+    track of their score as they progress through the quiz.`,
+    "source code": "https://github.com/jamesekunola/quiz-app",
+  },
+  {
+    img: "/assets/ecom.jpg",
+    web: false,
+    name: "ecommerce product page ",
+    desc: ` The app works by presenting the user with a series of questions, retrieved from an API, along with
+    multiple choice answers. The user can select an answer for each question, and the app will keep
+    track of their score as they progress through the quiz.`,
+    "source code": "https://github.com/jamesekunola/quiz-app",
+  },
+  {
+    img: "/assets/url.jpg",
+    web: false,
+    name: "URL links shortner landing page",
+    desc: ` The app works by presenting the user with a series of questions, retrieved from an API, along with
+    multiple choice answers. The user can select an answer for each question, and the app will keep
+    track of their score as they progress through the quiz.`,
+    "source code": "https://github.com/jamesekunola/quiz-app",
+  },
+];
+const projectEl = document.querySelector(".projects-page-center");
+const sectionEl = document.querySelectorAll(".revel");
+
 // function
 const displayMobileNav = () => {
   const hambugerBtn = document.querySelector(".hambuger-menu");
@@ -12,6 +94,35 @@ const displayMobileNav = () => {
   closeNavBtn.addEventListener("click", () => {
     asideEl.classList.remove("show");
   });
+};
+
+const renderProjects = () => {
+  if (projectEl) {
+    projectEl.innerHTML = projects
+      .map((items) => {
+        return `<article class="single-project">
+    <div class="project-container">
+        <img src=${items.img} alt="project" />
+        <a href="#" class="project-icon">
+            <i class="fas fa-home"></i>
+        </a>
+    </div>
+    <div class="project-details">
+        <h4>${items.name}</h4>
+        <p>
+           ${items.desc}
+        </p>
+        <div class="project-footer">
+            <span>
+                <i class="fab fa-github"></i>
+            </span>
+            <a href=${items["source code"]}>source code</a>
+        </div>
+    </div>
+</article>`;
+      })
+      .join("");
+  }
 };
 
 const fixedNavbarOnScroll = () => {
@@ -29,6 +140,23 @@ const fixedNavbarOnScroll = () => {
   });
 };
 
+const revelElOnScroll = () => {
+  window.addEventListener("scroll", () => {
+    const pageHeight = window.innerHeight;
+    const eleTop = 150;
+    sectionEl.forEach((ele) => {
+      const eleTopPosition = ele.getBoundingClientRect().top;
+      if (eleTopPosition < pageHeight - eleTop) {
+        ele.classList.add("active");
+      } else {
+        ele.classList.remove("active");
+      }
+
+      console.log(eleTopPosition, pageHeight, pageHeight - 150);
+    });
+  });
+};
+
 const setFooterDate = () => {
   const dateEl = document.querySelector(".date");
   const date = new Date().getFullYear();
@@ -41,4 +169,6 @@ window.addEventListener("DOMContentLoaded", () => {
   setFooterDate();
   displayMobileNav();
   fixedNavbarOnScroll();
+  renderProjects();
+  revelElOnScroll();
 });
