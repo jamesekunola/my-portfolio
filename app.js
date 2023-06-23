62da78a9bd5ca3b91d607c8b707f2b0b7cf0670e
James Ekunola <jamesekunola@gmail.com>
2023-06-23 20:18:43 +0100

    added transition to all pages

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const projects = [
     img: "/assets/spacer.jpg",
     web: "https://space-tourist-travel.netlify.app/",
     name: "Space tourism multi-page website",
+    tools: ["css", "react"],
     desc: `The Space Tourism Multi-Page Website, developed with the help of React Router, presents a collection of pages exclusively designed for space tourism experiences.
 
 By leveraging React Router, this website provides users with seamless navigation capabilities, allowing them to effortlessly explore a range of captivating destinations, packages, and experiences within the realm of space tourism. With dynamic content rendering based on selected routes, users enjoy a smooth and uninterrupted browsing experience throughout their exploration of the site.`,
@@ -13,6 +14,7 @@ By leveraging React Router, this website provides users with seamless navigation
     img: "/assets/url.jpg",
     web: "https://url-linkshortner.netlify.app/index.html",
     name: "URL shortening API landing page",
+    tools: ["html", "scss", "js", "api"],
     desc: ` The URL Shortening app is a user-facing application that provides a seamless and intuitive interface for users to shorten long URLs. With this app, users can input their lengthy URLs into a designated field and receive a shortened version as the output. The frontend app leverages technologies such as HTML, CSS, and JavaScript to create an interactive and responsive user interface.
 `,
     "source code": "https://github.com/jamesekunola/space-tourist-website",
@@ -22,6 +24,7 @@ By leveraging React Router, this website provides users with seamless navigation
     img: "/assets/ip-track.jpg",
     web: "https://ip-add-finder.netlify.app/",
     name: "IP Address Tracker",
+    tools: ["html", "scss", "js", "api"],
     desc: `The IP address tracker app is a useful tool that allows users to track and obtain information about any given IP address. With this app, users can input an IP address and retrieve details such as the geographical location, internet service provider (ISP), time zone, and other relevant information associated with that IP address. The app utilizes APIs and database resources to accurately fetch and display the desired information in a user-friendly manner`,
     "source code": "https://github.com/jamesekunola/ip-address-tracker-master",
   },
@@ -30,6 +33,7 @@ By leveraging React Router, this website provides users with seamless navigation
     img: "/assets/interative.jpg",
     web: `https://interactivecommentssection.netlify.app/`,
     name: "Interactive comments section",
+    tools: ["css", " react"],
     desc: `The Interactive Comment Section app was built using React and implemented the use of the Context API. This powerful combination allows for efficient state management and seamless data sharing throughout the app. With React's component-based architecture and the Context API's ability to provide centralized data access, the app ensures smooth interactions and real-time updates within the comment section. Users can perform actions like deleting, replying, creating, and editing comments with ease, thanks to the robust foundation provided by React and the Context API.`,
     "source code": "https://github.com/jamesekunola/quiz-app",
   },
@@ -37,6 +41,7 @@ By leveraging React Router, this website provides users with seamless navigation
     img: "/assets/work_rest_country_api.jpg",
     web: "https://dainty-bombolone-d7d98f.netlify.app/",
     name: "REST Countries API with color theme switcher",
+    tools: ["css", "react", "api"],
     desc: ` 
 The REST Countries API with Color Theme Switcher is an advanced web application built using React and React Router. It combines the power of the REST Countries API, a color theme switcher, and React Router to create a seamless and engaging user experience.
 With React Router, the app enables navigation between different pages. When a user selects a country card, the app retrieves the selected card's ID using React Router's useParams hook. It then dynamically renders the details of the selected country on a separate page.`,
@@ -47,6 +52,7 @@ With React Router, the app enables navigation between different pages. When a us
     img: "/assets/easy.jpg",
     web: "https://famous-pony-b8a4b3.netlify.app/",
     name: "The EasyBank Landing Page",
+    tools: ["css", "react"],
     desc: ` The EasyBank Landing Page is a modern and visually appealing website designed to showcase the features and offerings of the EasyBank financial institution. The landing page serves as an entry point for users, providing them with an overview of the bank's services and encouraging them to explore further.`,
     "source code": "https://github.com/jamesekunola/quiz-app",
   },
@@ -54,6 +60,7 @@ With React Router, the app enables navigation between different pages. When a us
     img: "/assets/multi-step.jpg",
     web: null,
     name: "Multi-step form",
+    tools: ["react", "redux"],
     desc: `I am currently working on a Multi-step Form project that leverages Redux for state management. With Redux, the form's state is effectively managed and synchronized throughout the different steps. This allows users to navigate back to previous steps, make necessary corrections, and observe the real-time updates of their changes. By utilizing Redux, the form maintains a consistent and centralized data flow, enhancing the user experience and ensuring seamless interaction within the form.`,
     "source code": "https://github.com/jamesekunola/multi-step-form",
   },
@@ -90,6 +97,16 @@ const renderProjects = () => {
     </div>
     <div class="project-details">
         <h4>${items.name}</h4>
+       
+        <ul class="tools">
+        ${items.tools
+          .map((tool) => {
+            return `<li class=${tool}>${tool}</li>`;
+          })
+          .join("")}
+              
+          </ul>
+
         <p>
            ${items.desc}
         </p>
@@ -133,7 +150,7 @@ const revelElOnScroll = () => {
         ele.classList.remove("active");
       }
 
-      console.log(eleTopPosition, pageHeight, pageHeight - 150);
+      // console.log(eleTopPosition, pageHeight, pageHeight - 150);
     });
   });
 };
